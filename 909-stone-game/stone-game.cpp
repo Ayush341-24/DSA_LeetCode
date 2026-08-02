95ac02c7d167636460884fdc822bcaec5779b6a3
Ayush Ranjan <ayushranjan1500@gmail.com>
2026-08-02 14:46:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (46.05%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+        // Alice can get all the sum of even indexes or odd indexes, 
+        // She will win every time.
+    }
+};
