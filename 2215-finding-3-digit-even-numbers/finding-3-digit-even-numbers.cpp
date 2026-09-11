220c4843b7b22e225eb6a3713b1357d8af7d331e
Ayush Ranjan <ayushranjan1500@gmail.com>
2026-09-11 18:29:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (95.32%) - LeetSync

---
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        vector<int> mp;
+        vector<int> count(10 , 0);
+
+        for(int val : digits){
+            count[val]++;
+        }
+
+        for(int i=1; i<=9; i++){
+            if(count[i] == 0)continue;
+            count[i]--;
+            for(int j=0; j<=9; j++){
+                if(count[j] == 0)continue;
+                count[j]--;
+                for(int k=0; k<=8; k+=2){
+                    if(count[k] == 0)continue;
+                    count[k]--;
+
+                    int digit = 100 * i + 10 * j + k;
+                    mp.push_back(digit);
+                    count[k]++;
+                }
+                count[j]++;
+            }
+            count[i]++;   
+        }
+        return mp;
+    }
+};
